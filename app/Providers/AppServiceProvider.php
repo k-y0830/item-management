bf8a326aa2229dbd23f8fe85584994da4307ef63
k-y0830 <v.pp0830@gmail.com>
2023-12-13 15:26:15 +0900

    change https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,5 +20,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
+        
+        if (\App::environment(['production'])) {
+            \URL::forceScheme('https');
+        }
     }
 }
